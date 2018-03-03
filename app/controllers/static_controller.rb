84d57897664e7c16a051fb40e7500cf34fd12e3b
Min Kim <mk4555@nyu.edu>
2018-03-03 22:11:27 +0000

    automatically backed up by learn

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,10 @@
+class StaticController < ApplicationController
+  def home
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- db/migrate/20140703140419_create_rides.rb
+++ db/migrate/20140703140419_create_rides.rb
@@ -1,3 +1,7 @@
 class CreateRides < ActiveRecord::Migration
   # Write your migrations here
+  create_table :rides do |t|
+    t.integer :attraction_id
+    t.integer :user_id
+  end
 end
